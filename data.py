8df66e04036a921e1fcce23de46941d06371f2d9
Aiden-L2 <Aidenrexl@nycstudents.net>
2026-09-22 09:22:29 -0400

    Create data.py

---
+++ data.py
@@ -0,0 +1,17 @@
+""" x = 5
+y = float(5)
+print(x,y) """
+
+""" values = [1,2,4,6,8,10,15,20]
+print(values)
+for i in values:
+    print(i)
+    print(values[0])
+    print(values[6])
+    print(values[7]) """
+
+x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z)
